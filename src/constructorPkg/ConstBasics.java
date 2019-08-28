af49f51a92d1cea207832ec2ff10ac479560e16c
sharadpok <sharadpokale11@gmail.com>
2019-08-28 08:32:01 +0530

    Basics Of Java

---
+++ src/constructorPkg/ConstBasics.java
@@ -0,0 +1,16 @@
+package constructorPkg;
+
+public class ConstBasics
+{
+    ConstBasics()
+   {
+	System.out.println("I am in constructor");   
+   }
+public static void main(String[] args)
+	{
+	ConstBasics obj=new ConstBasics();
+	
+
+	}
+
+}
